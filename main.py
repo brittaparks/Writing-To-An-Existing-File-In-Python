0798b8a91721537974dffee7cbe184083c6e270e
brittaparks <brittaparks@gmail.com>
2026-06-13 13:27:00 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#I am adding a new line character at the beginning, because I remmeber not adding a 
+#new line character to the end of the last line when I created the file.
+
+with open("Python_code_1.txt", "a") as candy:
+    candy.write("\nThis should make the third line")
